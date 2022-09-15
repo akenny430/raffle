b864e3d74727166188358f5e50ef7c153d139b25
Aiden Kenny <akenny430@gmail.com>
2022-09-15 01:28:57 -0400

    added constants

---
+++ src/constants.hpp
@@ -0,0 +1,28 @@
+# ifndef CONSTANTS_HPP 
+# define CONSTANTS_HPP 
+
+namespace prob
+{
+    constexpr float p1 = 0.15f; 
+    constexpr float p2 = 0.10f; 
+    constexpr float p3 = 0.30f; 
+    constexpr float p4 = 0.45f; 
+}
+
+namespace count 
+{
+    constexpr int c1 = 5;
+    constexpr int c2 = 20;
+    constexpr int c3 = 100;
+    constexpr int c4 = 300;
+}
+
+namespace val 
+{
+    constexpr float v1 = 2.0f;
+    constexpr float v2 = 1.0f;
+    constexpr float v3 = 0.4f;
+    constexpr float v4 = 0.333f;
+}
+
+#endif
