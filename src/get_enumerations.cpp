491f273d26d701fb0632e8858d10b127007f82d9
Aiden Kenny <akenny430@gmail.com>
2022-09-16 01:18:20 -0400

    updated enumerations

--- src/get_enumerations.cpp
+++ src/get_enumerations.cpp
@@ -1,5 +1,9 @@
 #include <iostream> 
 
+// #include "constants.hpp" 
+#include "factorial.hpp" 
+#include "multinomial.hpp" 
+
 
 void get_enumerations(const int n)
 {
@@ -8,6 +12,7 @@ void get_enumerations(const int n)
 	int temp1, temp2, temp3, temp4; 
 	int count = 0; 
 
+	float density = 0.0f; 
 	temp1 = n; 
 	while (temp1 >= 0) 
 	{
@@ -27,13 +32,15 @@ void get_enumerations(const int n)
 					--temp4; 
 					if (n1 + n2 + n3 + n4 != n) {continue;}
 					// do something here to return the values 
-					// std::cout << "(" 
-					// << n1 << ", "
-					// << n2 << ", "
-					// << n3 << ", "
-					// << n4 << ")"
-					// << "\n"; 
+					std::cout << "(" 
+					<< n1 << ", "
+					<< n2 << ", "
+					<< n3 << ", "
+					<< n4 << "): "
+					<< factorial(n1, n2, n3, n4) 
+					<< "\n"; 
 					++count; 
+					density += multinomial_pdf(n1, n2, n3, n4); 
 				} 
 				--temp3; 
 			}
@@ -41,13 +48,14 @@ void get_enumerations(const int n)
 		}
 		--temp1; 
 	}
-	std::cout << "# of enumerations for " << n << ": " << count << "\n"; 
+	std::cout << "# of enumerations for " << n << ": " << count << " (" << 100 * density << "%)\n"; 
 }
 
 
 int main()
 {
-	for (int x : {5, 10, 15, 20, 30}) {get_enumerations(x);}
+	// for (int x : {5, 10, 15, 20, 30}) {get_enumerations(x);}
+	for (int x : {2, 3, 5}) {get_enumerations(x);}
 	// get_enumerations(20); 
 	return 0; 
 }
